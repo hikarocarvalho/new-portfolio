9e7b4c0fae13b8e814c96c65f215c6386c39eb4b
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-01-04 21:46:39 -0300

    feat(entity) : add html

--- src/Entitys/progamimgLanguages/html.js
+++ src/Entitys/progamimgLanguages/html.js
@@ -1,10 +1,250 @@
-const html = [
-    {
-        name:"",
-        topic:[
+const html = [{
+        name: "HTML Tutorial",
+        topic: [
+
             {
-                name:"",
-                checked:false
+                name: "HTML HOME",
+                checked: false
+            },
+            {
+                name: "HTML Introduction",
+                checked: false
+            },
+            {
+                name: "HTML Editors",
+                checked: false
+            },
+            {
+                name: "HTML Basic",
+                checked: false
+            },
+            {
+                name: "HTML Elements",
+                checked: false
+            },
+            {
+                name: "HTML Attributes",
+                checked: false
+            },
+            {
+                name: "HTML Headings",
+                checked: false
+            },
+            {
+                name: "HTML Paragraphs",
+                checked: false
+            },
+            {
+                name: "HTML Styles",
+                checked: false
+            },
+            {
+                name: "HTML Formatting",
+                checked: false
+            },
+            {
+                name: "HTML Quotations",
+                checked: false
+            },
+            {
+                name: "HTML Comments",
+                checked: false
+            },
+            {
+                name: "HTML Colors",
+                checked: false
+            },
+            {
+                name: "HTML CSS",
+                checked: false
+            },
+            {
+                name: "HTML Links",
+                checked: false
+            },
+            {
+                name: "HTML Images",
+                checked: false
+            },
+            {
+                name: "HTML Favicon",
+                checked: false
+            },
+            {
+                name: "HTML Tables",
+                checked: false
+            },
+            {
+                name: "HTML Lists",
+                checked: false
+            },
+            {
+                name: "HTML Block & Inline",
+                checked: false
+            },
+            {
+                name: "HTML Classes",
+                checked: false
+            },
+            {
+                name: "HTML Id",
+                checked: false
+            },
+            {
+                name: "HTML Iframes",
+                checked: false
+            },
+            {
+                name: "HTML JavaScript",
+                checked: false
+            },
+            {
+                name: "HTML File Paths",
+                checked: false
+            },
+            {
+                name: "HTML Head",
+                checked: false
+            },
+            {
+                name: "HTML Layout",
+                checked: false
+            },
+            {
+                name: "HTML Responsive",
+                checked: false
+            },
+            {
+                name: "HTML Computercode",
+                checked: false
+            },
+            {
+                name: "HTML Semantics",
+                checked: false
+            },
+            {
+                name: "HTML Style Guide",
+                checked: false
+            },
+            {
+                name: "HTML Entities",
+                checked: false
+            },
+            {
+                name: "HTML Symbols",
+                checked: false
+            },
+            {
+                name: "HTML Emojis",
+                checked: false
+            },
+            {
+                name: "HTML Charset",
+                checked: false
+            },
+            {
+                name: "HTML URL Encode",
+                checked: false
+            },
+            {
+                name: "HTML vs. XHTML",
+                checked: false
+            }
+        ]
+    },
+    {
+        name: "HTML Forms",
+        topic: [
+
+            {
+                name: "HTML Forms",
+                checked: false
+            },
+            {
+                name: "HTML Form Attributes",
+                checked: false
+            },
+            {
+                name: "HTML Form Elements",
+                checked: false
+            },
+            {
+                name: "HTML Input Types",
+                checked: false
+            },
+            {
+                name: "HTML Input Attributes",
+                checked: false
+            },
+            {
+                name: "HTML Input Form Attributes",
+                checked: false
+            }
+        ]
+    },
+    {
+        name: "HTML Graphics",
+        topic: [
+
+            {
+                name: "HTML Canvas",
+                checked: false
+            },
+            {
+                name: "HTML SVG",
+                checked: false
+            },
+        ]
+    },
+    {
+        name: "HTML Media",
+        topic: [
+
+            {
+                name: "HTML Media",
+                checked: false
+            },
+            {
+                name: "HTML Video",
+                checked: false
+            },
+            {
+                name: "HTML Audio",
+                checked: false
+            },
+            {
+                name: "HTML Plug-ins",
+                checked: false
+            },
+            {
+                name: "HTML YouTube",
+                checked: false
+            }
+        ]
+    },
+    {
+        name: "HTML APIs",
+        topic: [
+
+            {
+                name: "HTML Geolocation",
+                checked: false
+            },
+            {
+                name: "HTML Drag/Drop",
+                checked: false
+            },
+            {
+                name: "HTML Web Storage",
+                checked: false
+            },
+            {
+                name: "HTML Web Workers",
+                checked: false
+            },
+            {
+                name: "HTML SSE",
+                checked: false
             }
         ]
     }
