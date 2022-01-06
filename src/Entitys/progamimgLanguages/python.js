3e0c8da69c096bd857c21c4e25c0be7d6027f325
hikarocarvalho <hikarofcarvalho@gmail.com>
2022-01-06 09:19:07 -0300

    refactor[update]:update technologies entity

--- src/Entitys/progamimgLanguages/python.js
+++ src/Entitys/progamimgLanguages/python.js
@@ -1,143 +1,389 @@
-const python = [
-    
- {
-    name:"Basic",
-    topic:[
-            {
-                name:"Basic Syntax",
-                checked:false
+const python = [{
+        name: "Basic",
+        topic: [{
+                name: "Syntax",
+                checked: false
             },
             {
-                name:"Variables and Data Types",
-                checked:false
+                name: "Comments",
+                checked: false
             },
             {
-                name:"Conditionals",
-                checked:false
+                name: "Variables",
+                checked: false
             },
             {
-                name:"Type Casting, Exceptions",
-                checked:false
+                name: "Data Types",
+                checked: false
             },
             {
-                name:"Functions, Builtin Functions",
-                checked:false
+                name: "Numbers",
+                checked: false
             },
             {
-                name:"lists, tuples, Sets, Dictionaries",
-                checked:false
+                name: "Casting",
+                checked: false
+            },
+            {
+                name: "Strings",
+                checked: false
+            },
+            {
+                name: "Booleans",
+                checked: false
+            },
+            {
+                name: "Operators",
+                checked: false
+            },
+            {
+                name: "Lists",
+                checked: false
+            },
+            {
+                name: "Tuples",
+                checked: false
+            },
+            {
+                name: "Sets",
+                checked: false
+            },
+            {
+                name: "Dictionaries",
+                checked: false
+            },
+            {
+                name: "If...Else",
+                checked: false
+            },
+            {
+                name: "While Loops",
+                checked: false
+            },
+            {
+                name: "For Loops",
+                checked: false
+            },
+            {
+                name: "Functions",
+                checked: false
+            },
+            {
+                name: "Lambda",
+                checked: false
+            },
+            {
+                name: "Arrays",
+                checked: false
+            },
+            {
+                name: "Classes/Objects",
+                checked: false
+            },
+            {
+                name: "Inheritance",
+                checked: false
+            },
+            {
+                name: "Iterators",
+                checked: false
+            },
+            {
+                name: "Scope",
+                checked: false
+            },
+            {
+                name: "Modules",
+                checked: false
+            },
+            {
+                name: "Dates",
+                checked: false
+            },
+            {
+                name: "Math",
+                checked: false
+            },
+            {
+                name: "JSON",
+                checked: false
+            },
+            {
+                name: "RegEx",
+                checked: false
+            },
+            {
+                name: "PIP",
+                checked: false
+            },
+            {
+                name: "Try...Except",
+                checked: false
+            },
+            {
+                name: "User Input",
+                checked: false
+            },
+            {
+                name: "String Formatting",
+                checked: false
+            }
+
+        ]
+    },
+    {
+        name: "File Handling",
+        topic: [{
+                name: "File Handling",
+                checked: false
+            },
+            {
+                name: "Read Files",
+                checked: false
+            },
+            {
+                name: "Write/Create Files",
+                checked: false
+            },
+            {
+                name: "Delete Files",
+                checked: false
+            }
+
+        ]
+    },
+    {
+        name: "Modules",
+        topic: [{
+                name: "NumPy Tutorial",
+                checked: false
+            },
+            {
+                name: "Pandas Tutorial",
+                checked: false
+            },
+            {
+                name: "SciPy Tutorial",
+                checked: false
+            }
+
+
+        ]
+    },
+    {
+        name: "Matplotlib",
+        topic: [{
+                name: "Intro",
+                checked: false
+            },
+            {
+                name: "Get Started",
+                checked: false
+            },
+            {
+                name: "Pyplot",
+                checked: false
+            },
+            {
+                name: "Plotting",
+                checked: false
+            },
+            {
+                name: "Markers",
+                checked: false
+            },
+            {
+                name: "Line",
+                checked: false
+            },
+            {
+                name: "Labels",
+                checked: false
+            },
+            {
+                name: "Grid",
+                checked: false
+            },
+            {
+                name: "Subplots",
+                checked: false
+            },
+            {
+                name: "Scatter",
+                checked: false
+            },
+            {
+                name: "Bars",
+                checked: false
+            },
+            {
+                name: "Histograms",
+                checked: false
+            },
+            {
+                name: "Pie Charts",
+                checked: false
+            }
+
+
+        ]
+    },
+    {
+        name: "Machine Learning",
+        topic: [{
+                name: "Mean Median Mode",
+                checked: false
+            },
+            {
+                name: "Standard Deviation",
+                checked: false
+            },
+            {
+                name: "Percentile",
+                checked: false
+            },
+            {
+                name: "Data Distribution",
+                checked: false
+            },
+            {
+                name: "Normal Data Distribution",
+                checked: false
+            },
+            {
+                name: "Scatter Plot",
+                checked: false
+            },
+            {
+                name: "Linear Regression",
+                checked: false
+            },
+            {
+                name: "Polynomial Regression",
+                checked: false
+            },
+            {
+                name: "Multiple Regression",
+                checked: false
+            },
+            {
+                name: "Scale",
+                checked: false
+            },
+            {
+                name: "Train/Test",
+                checked: false
+            },
+            {
+                name: "Decision Tree",
+                checked: false
+            }
+
+        ]
+    },
+
+    {
+        name: "MySQL",
+        topic: [{
+                name: "Get Started",
+                checked: false
+            },
+            {
+                name: "Create Database",
+                checked: false
+            },
+            {
+                name: "Create Table",
+                checked: false
+            },
+            {
+                name: "Insert",
+                checked: false
+            },
+            {
+                name: "Select",
+                checked: false
+            },
+            {
+                name: "Where",
+                checked: false
+            },
+            {
+                name: "Order By",
+                checked: false
+            },
+            {
+                name: "Delete",
+                checked: false
+            },
+            {
+                name: "Drop Table",
+                checked: false
+            },
+            {
+                name: "Update",
+                checked: false
+            },
+            {
+                name: "Limit",
+                checked: false
+            },
+            {
+                name: "Join",
+                checked: false
             }
         ]
     },
     {
-    name:"Advanced topics",
-    topic:[
-            {
-                name:"Classes",
-                checked:false
+        name: "MongoDB",
+        topic: [{
+                name: "Get Started",
+                checked: false
             },
             {
-                name:"Inheritance",
-                checked:false
+                name: "Create Database",
+                checked: false
             },
             {
-                name:"Methods, Dunder",
-                checked:false
+                name: "Create Collection",
+                checked: false
             },
             {
-                name:"OOP",
-                checked:false
+                name: "Insert",
+                checked: false
             },
             {
-                name:"Regular Expressions",
-                checked:false
+                name: "Find",
+                checked: false
             },
             {
-                name:"Decorators",
-                checked:false
+                name: "Query",
+                checked: false
             },
             {
-                name:"Lambdas",
-                checked:false
+                name: "Sort",
+                checked: false
             },
             {
-                name:"Modules",
-                checked:false
+                name: "Delete",
+                checked: false
             },
             {
-                name:"Iterators",
-                checked:false
-            }
-        ]
-    },
-    {
-    name:"Repo Hosting Services",
-    topic:[
-            {
-                name:"Github",
-                checked:false
+                name: "Drop Collection",
+                checked: false
             },
             {
-                name:"Gitlab",
-                checked:false
+                name: "Update",
+                checked: false
             },
             {
-                name:"Bitbucket",
-                checked:false
-            }
-        ]
-    },
-    {
-    name:"Pakage Managers",
-    topic:[
-            {
-                name:"PyPI",
-                checked:false
+                name: "Limit",
+                checked: false
             },
-            {
-                name:"Pip",
-                checked:false
-            }
-        ]
-    },
-    {
-    name:"Framework",
-    topic:[
-            { 
-                name:"Django",
-                checked:false
-            },
-            {
-                name:"Flask",
-                checked:false
-            },
-            {
-                name:"Pyramid",
-                checked:false
-            }
-        ]
-    },
-    {
-        name:"Testes",
-        topic:[
-            {
-                    name:"pytest",
-                    checked:false
-            },
-            {
-                    name:"doctest",
-                    checked:false
-            },
-            {
-                    name:"unittest / pyUnit",
-                    checked:false
-            },
-            {
-                    name:"nose",
-                    checked:false
-            }
         ]
     }
+
+
 ];
 
 export default python;
