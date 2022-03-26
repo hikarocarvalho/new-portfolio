f7301d7bde45eee205ed1016de5e750aea365438
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-03-25 22:58:08 -0300

    fix(project): fix links

--- src/components/ProjectListItem/ProjectListItem.js
+++ src/components/ProjectListItem/ProjectListItem.js
@@ -1,42 +1,50 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import './ProjectListItem.css';
-import github from './../../imgs/github-logo_icon-icons.com_73546.svg';
-import video from './../../imgs/film_projector_cinema_icon-icons.com_66132.svg';
-export default function ProjectListItem(props){
-    const [itemList,setItem] = useState();
-    useEffect(()=>{
-        setItem({
-            ...itemList,
-            items:props.projectList,
-            stack:props.stack
-        })
-    },[props.projectList]);
-    return (
-        <div className="list-box">
-            {itemList?
-            itemList.items.map((item,index)=>(
-                <NavLink to={"/project/"+item.id+"/"+props.stack} key={index} className="project-item">
-                   
-                        <figure className="project-figure">
-                            <img src={item.image} alt="teste" className="screen-view"></img>
-                        </figure>
-                        <article className="project-description">
-                            <p className="project-title">
-                                    {item.name}
-                            </p>
-                            <p className="project-text">
-                                    {item.description}
-                            </p>
-                        </article>
-                        <footer className="links">
-                            <i><a href={item.github}><img className="icons" src={github} alt="icon" /></a></i>
-                            <i><a href={item.video}><img className="icons" src={video} alt="icon" /></a></i>
-                        </footer>
-                 
-                </NavLink>
-            ))
-            :null}
-        </div>
-    );
+import "./ProjectListItem.css";
+import github from "./../../imgs/github-logo_icon-icons.com_73546.svg";
+import video from "./../../imgs/film_projector_cinema_icon-icons.com_66132.svg";
+export default function ProjectListItem(props) {
+  const [itemList, setItem] = useState();
+  useEffect(() => {
+    setItem({
+      ...itemList,
+      items: props.projectList,
+      stack: props.stack,
+    });
+  }, [props.projectList]);
+  return (
+    <div className="list-box">
+      {itemList
+        ? itemList.items.map((item, index) => (
+            <div className="project-item" key={index}>
+              <NavLink to={"/project/" + item.id + "/" + props.stack}>
+                <figure className="project-figure">
+                  <img
+                    src={item.image}
+                    alt="teste"
+                    className="screen-view"
+                  ></img>
+                </figure>
+                <article className="project-description">
+                  <p className="project-title">{item.name}</p>
+                  <p className="project-text">{item.description}</p>
+                </article>
+              </NavLink>
+              <footer className="links">
+                <i>
+                  <a href={item.github}>
+                    <img className="icons" src={github} alt="icon" />
+                  </a>
+                </i>
+                <i>
+                  <a href={item.video}>
+                    <img className="icons" src={video} alt="icon" />
+                  </a>
+                </i>
+              </footer>
+            </div>
+          ))
+        : null}
+    </div>
+  );
 }
